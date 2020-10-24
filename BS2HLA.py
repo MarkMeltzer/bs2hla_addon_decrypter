4d329c1a6fd354bf0f88d06076904c4f34a11c67
Nicolás Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2020-10-24 13:34:42 -0300

    Added functionality to copy decrypted addon directly to game folder.

--- BS2HLA.py
+++ BS2HLA.py
@@ -1,5 +1,6 @@
 from os import system
 import os
+import zipfile
 import tkinter as tkin
 import tkinter.scrolledtext as st
 from tkinter import font
@@ -29,8 +30,12 @@ def decrypt():
     # Get the Bioshock root path and addon zip path
     Bioshock_root_path = Path(Bioshock_root_path_entry.get().strip())
     addon_zip_path = Path(addon_zip_path_entry.get().strip())
+    HLA_root_path = Path(HLA_root_path_entry.get().strip())
     if not Bioshock_root_path.is_dir():
         print_to_textbox(f"{Bioshock_root_path} cannot be found, try again.",output)
+        return 1
+    if not HLA_root_path.is_dir():
+        print_to_textbox(f"{HLA_root_path} cannot be found, try again.",output)
         return 1
     if not addon_zip_path.is_file():
         print_to_textbox(f"{addon_zip_path} cannot be found or is not a file, try again.",output)
@@ -65,29 +70,37 @@ def decrypt():
     decrypted_addon_zip = bytearray(addon_zip_size)
     start_time = time.time()
 
-    def decrypt_addon_start():
-        for i in range(addon_zip_size):
-            if i >= len(assets):
-                # if assets are smaller than addon zip, pad the assets with 0 bits
-                decrypted_addon_zip[i] = 0 ^ addon_zip[i]
-            else:
-                decrypted_addon_zip[i] = assets[i] ^ addon_zip[i]
+    def decrypt_addon_start(HLA_root_path):
+        def _decrypt_addon_start():
+            for i in range(addon_zip_size):
+                if i >= len(assets):
+                    # if assets are smaller than addon zip, pad the assets with 0 bits
+                    decrypted_addon_zip[i] = 0 ^ addon_zip[i]
+                else:
+                    decrypted_addon_zip[i] = assets[i] ^ addon_zip[i]
 
-            # print progress
-            if i % 10000000 == 0:
-                print_to_textbox(f"{(i / addon_zip_size * 100):.2f}% done", output)
+                # print progress
+                if i % 10000000 == 0:
+                    print_to_textbox(f"{(i / addon_zip_size * 100):.2f}% done", output)
 
-        print_to_textbox(f"Addon decrypted! Decryption took {time.time() - start_time:.1f} seconds.", output)
+            print_to_textbox(f"Addon decrypted! Decryption took {time.time() - start_time:.1f} seconds.", output)
+            decrypt_addon_finish(HLA_root_path)
+        return _decrypt_addon_start
 
-        decrypt_addon_finish()
-
-    def decrypt_addon_finish():
+    def decrypt_addon_finish(HLA_root_path):
+        addon_folder = HLA_root_path + "\game\hlvr_addons"
         # Save the decrypted addon
         print_to_textbox("Saving addon....", output)
         try:
             open(addon_zip_path, 'wb').write(decrypted_addon_zip)
+            if not os.path.exists(addon_folder):
+                os.makedirs(addon_folder)
+            with zipfile.ZipFile(addon_zip_path, 'r') as zip_ref:
+                zip_ref.extractall(addon_folder)
         except Exception as e:
             print_to_textbox("Could not save addon.", output)
+            print_to_textbox(str(addon_zip_path), output)
+            print_to_textbox(str(addon_folder), output)
             print_to_textbox(str(e), output)
             return 1
         print_to_textbox("Decrypted addon saved!", output)
@@ -97,7 +110,7 @@ def decrypt():
         decrypt_button['state'] = tkin.NORMAL
 
     # do the decryption on a different thread from the GUI thread
-    XOR_tread = threading.Thread(target=decrypt_addon_start)
+    XOR_tread = threading.Thread(target=decrypt_addon_start(str(HLA_root_path.resolve())))
     XOR_tread.start()
     decrypt_button['state'] = tkin.DISABLED
 
@@ -123,13 +136,18 @@ addon_zip_path_entry_label.grid(row=1, sticky=tkin.W, ipadx=5)
 addon_zip_path_entry = tkin.Entry(window, width=75)
 addon_zip_path_entry.grid(row=1, column=1)
 
+HLA_root_path_entry_label = tkin.Label(window, text='HLA installation root folder: ')
+HLA_root_path_entry_label.grid(row=2, sticky=tkin.W, ipadx=5)
+HLA_root_path_entry = tkin.Entry(window, width=75)
+HLA_root_path_entry.grid(row=2, column=1)
+
 # button
 decrypt_button = tkin.Button(window, text="Decrypt!", command=decrypt, font=font.Font(size=12))
-decrypt_button.grid(row=2, columnspan=2, pady=10)
+decrypt_button.grid(row=3, columnspan=2, pady=10)
 
 # Text output
 output = st.ScrolledText(window, wrap=tkin.WORD, bg="black", fg="#e8e8e8")
-output.grid(row=3, columnspan=2, padx=5)
+output.grid(row=4, columnspan=2, padx=5)
 output.configure(state='disabled')
 
 window.mainloop()
