e98334d623ccfc8a7e1fe0abd7ff5600c076a4e9
Mathias <m.kirkeng@gmail.com>
2020-10-24 14:52:16 +0200

    Initial commit.

---
+++ BS2HLA.py
@@ -0,0 +1,135 @@
+from os import system
+import os
+import tkinter as tkin
+import tkinter.scrolledtext as st
+from tkinter import font
+import time
+from pathlib import Path
+import threading
+
+# Used to allow pyinstaller to find resource files from a relative path
+# src: https://stackoverflow.com/a/13790741/
+def resource_path(relative):
+    return os.path.join(
+        os.environ.get(
+            "_MEIPASS2",
+            os.path.abspath(".")
+        ),
+        relative
+    )
+
+def print_to_textbox(s, textbox):
+    textbox.configure(state='normal')
+    textbox.insert(tkin.END, s+"\n")
+    textbox.configure(state='disabled')
+    textbox.see(tkin.END)
+    window.update_idletasks()
+
+def decrypt():
+    # Get the Bioshock root path and addon zip path
+    Bioshock_root_path = Path(Bioshock_root_path_entry.get().strip())
+    addon_zip_path = Path(addon_zip_path_entry.get().strip())
+    if not Bioshock_root_path.is_dir():
+        print_to_textbox(f"{Bioshock_root_path} cannot be found, try again.",output)
+        return 1
+    if not addon_zip_path.is_file():
+        print_to_textbox(f"{addon_zip_path} cannot be found or is not a file, try again.",output)
+        return 1
+
+    # load the bioshock assets
+    print_to_textbox("Loading Bioshock assets...", output)
+    try:
+        bulkfile = bytearray(open(Bioshock_root_path / "Content" / "BulkContent" / "1-medicalLevel.blk", 'rb').read())
+        mapfile = bytearray(open(Bioshock_root_path / "Content" / "Maps" / "1-Medical.bsm", 'rb').read())
+        assets = bulkfile + mapfile
+        assets_size = len(assets)
+        print_to_textbox(f"Bioshock assets loaded! Size: {(assets_size / 1000000):.2f}mb", output)
+    except Exception as e:
+        print_to_textbox("Could not load Bioshock assets.", output)
+        print_to_textbox(str(e), output)
+        return 1
+
+    # load the addon zip
+    print_to_textbox("Loading addon zip...", output)
+    try:
+        addon_zip = bytearray(open(addon_zip_path, 'rb').read())
+        addon_zip_size = len(addon_zip)
+        print_to_textbox(f"Addon zip loaded! Size: {(addon_zip_size / 1000000):.2f}mb", output)
+    except Exception as e:
+        print_to_textbox("Could not load the addon zip.", output)
+        print_to_textbox(str(e), output)
+        return 1
+
+    # decrypt addon by XOR-ing the addon zip with the asset files
+    print_to_textbox("Decrypting addon....", output)
+    decrypted_addon_zip = bytearray(addon_zip_size)
+    start_time = time.time()
+
+    def decrypt_addon_start():
+        for i in range(addon_zip_size):
+            if i >= len(assets):
+                # if assets are smaller than addon zip, pad the assets with 0 bits
+                decrypted_addon_zip[i] = 0 ^ addon_zip[i]
+            else:
+                decrypted_addon_zip[i] = assets[i] ^ addon_zip[i]
+
+            # print progress
+            if i % 10000000 == 0:
+                print_to_textbox(f"{(i / addon_zip_size * 100):.2f}% done", output)
+
+        print_to_textbox(f"Addon decrypted! Decryption took {time.time() - start_time:.1f} seconds.", output)
+
+        decrypt_addon_finish()
+
+    def decrypt_addon_finish():
+        # Save the decrypted addon
+        print_to_textbox("Saving addon....", output)
+        try:
+            open(addon_zip_path, 'wb').write(decrypted_addon_zip)
+        except Exception as e:
+            print_to_textbox("Could not save addon.", output)
+            print_to_textbox(str(e), output)
+            return 1
+        print_to_textbox("Decrypted addon saved!", output)
+        print_to_textbox("All done!", output)
+
+        # re-enable the button
+        decrypt_button['state'] = tkin.NORMAL
+
+    # do the decryption on a different thread from the GUI thread
+    XOR_tread = threading.Thread(target=decrypt_addon_start)
+    XOR_tread.start()
+    decrypt_button['state'] = tkin.DISABLED
+
+# Create tkiner window
+window = tkin.Tk()
+window.title("BS2HLA Medical Pavilion addon decrypter")
+try:
+    window.iconbitmap(resource_path('icon.ico'))
+except:
+    # no icon? no big deal
+    pass
+window.geometry("670x501")
+window.resizable(0,0)
+
+# Input text fields
+Bioshock_root_path_entry_label = tkin.Label(window, text="BioShock installation root folder: ")
+Bioshock_root_path_entry_label.grid(row=0, sticky=tkin.W, pady=7, ipadx=5)
+Bioshock_root_path_entry = tkin.Entry(window, width=75)
+Bioshock_root_path_entry.grid(row=0, column=1)
+
+addon_zip_path_entry_label = tkin.Label(window, text='The encrypted addon zip file: ')
+addon_zip_path_entry_label.grid(row=1, sticky=tkin.W, ipadx=5)
+addon_zip_path_entry = tkin.Entry(window, width=75)
+addon_zip_path_entry.grid(row=1, column=1)
+
+# button
+decrypt_button = tkin.Button(window, text="Decrypt!", command=decrypt, font=font.Font(size=12))
+decrypt_button.grid(row=2, columnspan=2, pady=10)
+
+# Text output
+output = st.ScrolledText(window, wrap=tkin.WORD, bg="black", fg="#e8e8e8")
+output.grid(row=3, columnspan=2, padx=5)
+output.configure(state='disabled')
+
+window.mainloop()
---
+++ readme.md
@@ -0,0 +1,36 @@
+![What an amazing logo, right?](/logo.png)
+
+# Description
+This little program lets you decrypt a Bioshock addon zip using the original Bioshock game files. **Neither this nor the encrypted addon zip contain any of the copyrighted original Bioshock assets**, you will need to provide these yourself by following the steps below. 
+
+The addon zip is encrypted by doing a bitwise XOR on the zip and a combination of Bioshock game files. Decrypting is done by doing another bitwise XOR on the encrypted zip and the same combination of Bioshock game files.
+
+# Requirements
+- BS2HLA.exe
+- An installation of Bioshock
+- The encrypted addon zip
+    - You can get this here: 
+
+# How to use
+1. Run the BS2HLA.exe (problems? See notes below)
+2. Provide the path to your Bioshock installation. For example: *C:\Program Files (x86)\Steam\steamapps\common\Bioshock*.
+3. Provide the path to the encrypted zip. For example: *C:\Program Files (x86)\Steam\steamapps\common\Half-Life Alyx\medical_pavilion_lobby.zip*.
+4. Hit decrypt and let er ripp.
+5. Unzip the contents of the decrypted addon zip into your Half Life Alyx *\content\hlvr_addons* folder. For example: *C:\Program Files (x86)\Steam\steamapps\common\Half-Life Alyx\content\hlvr_addons*.
+6. Select the map from "Local Addons" in game.
+7. Enjoy!
+
+# NOTES:
+- The zip does not contain any original Bioshock assets. You WILL need to provide these assets yourself or the data is useless.
+- It can take a couple of seconds for the program to start up, please be patient :)
+- Windows is not a fan of unknown executables so will throw up a smartscreen window. Just press "More info" and "Run anyway".
+- It's also possible that your anti-virus might see the program as a virus. You can add an exception for BS2HLA.exe or if you don't trust it use the instructions below to run the source python script directly.
+
+# Run the python script
+- Have python 3.6 or higher installed.
+- Make just python is added to the PATH enviroment variable.
+- Download the python script from this page (top right -> green "code" button -> Download zip)
+- Open a command prompt (Ctrl+R -> "cmd" -> Enter)
+- Use the following command: *cd* [path of the folder which contains the .py file]
+- Use the following command: *python bs2hla.py*
+- Follow steps from how to use from step 2 onwards.
